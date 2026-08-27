f9947daded6e4391b7ee840973bee4f2177f6e22
Voblit <michael.zakharov.physics@gmail.com>
2026-08-27 17:29:27 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,10 @@
-import { ScramjetFetchHandler, defaultConfig } from 'https://cdn.jsdelivr.net/npm/@petezah-games/scramjet@2.0.3-petezah-alpha/+esm';
+import {
+    route,
+    shouldRoute
+} from "https://esm.run/@petezah-games/scramjet-controller/worker";
 
-const handler = new ScramjetFetchHandler(defaultConfig);
-
-self.addEventListener('install', () => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', (event) => {
-  if (handler.route(event)) {
-    event.respondWith(handler.fetch(event));
-  }
+self.addEventListener("fetch", event => {
+    if (shouldRoute(event)) {
+        event.respondWith(route(event));
+    }
 });
